c68327241c499a32de61daf8ef09b81d55ac10bb
Milana Vodneva <158348785+kurseasy@users.noreply.github.com>
2026-09-18 21:46:47 +0300

    Update task-08.js

--- Node-Express-Nest/v0/solutions/task-08.js
+++ Node-Express-Nest/v0/solutions/task-08.js
@@ -1,3 +1,9 @@
 // Express.js static files serving for ToDo frontend
-// TODO: implement
-module.exports = {}; 
+const express = require('express');
+const path = require('path');
+
+const router = express.Router();
+
+router.use('/static', express.static(path.join(__dirname, 'public')));
+
+module.exports = router;
