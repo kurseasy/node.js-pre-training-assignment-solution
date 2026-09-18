f7e167c92ac76e59543ae8ec65eac1ceee13a062
Milana Vodneva <158348785+kurseasy@users.noreply.github.com>
2026-09-18 21:44:22 +0300

    Update task-05.ts

--- Node-Express-Nest/v0/solutions/task-05.ts
+++ Node-Express-Nest/v0/solutions/task-05.ts
@@ -1,10 +1,22 @@
 // NestJS Controller for /todos
 import { Controller, Get } from '@nestjs/common';
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 @Controller('todos')
 export class TodosController {
+  private todos: Todo[] = [
+    { id: 1, title: 'Buy milk', completed: false },
+    { id: 2, title: 'Walk the dog', completed: true },
+    { id: 3, title: 'Write code', completed: false },
+  ];
+
   @Get()
-  getTodos() {
-    // TODO: implement
+  getTodos(): Todo[] {
+    return this.todos;
   }
-} 
+}
